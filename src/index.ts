74701e264c2108a8ecef924573c086d033e87585
Laimonas Katkus <laimonas@katkus.eu>
2022-06-10 19:50:32 +0300

    laikajs-added player and npc exports for typings

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 export { Game, IGameConfig } from './Game';
 export { WebGlRenderer, CanvasRenderer } from './Renderers';
 export { WebAudioPlayer } from './AudioPlayers';
-export { IPlayerConfig, INpcConfig } from './Entity';
+export { IPlayerConfig, INpcConfig, Player, Npc } from './Entity';
 export { ILevelManagerConfig } from './LevelManager';
 export { IEvent, IEventsManagerConfig } from './EventManager';
